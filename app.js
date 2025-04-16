6cb4c807c3d5fb9db30b536d78f0edd9f9578380
Funsho Cornelius <funshocornelius95@gmail.com>
2025-04-16 15:35:21 +0100

    Urrrggghhhhhgit add .git add .

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -20,14 +21,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
-});
-
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
 app.use("/api/v1/auth", authRouter);
-
-app.use(errorMiddleware);
 
 export default app;
